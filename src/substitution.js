a9ba0c8d5b97a8bd619c2b008ed8c4a33b39d708
Dexter Berend <dexterjberend@gmail.com>
2021-08-08 16:55:43 -0500

    substitution

--- src/substitution.js
+++ src/substitution.js
@@ -7,7 +7,53 @@ const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    const firstAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    if (alphabet.length !== 26) return false
+    if (!checkAlphabet(alphabet)) return false
+
+    input = input.toLowerCase()
+    const words = input.split(' ')
+    if (encode) {
+      return words.map(word => encodeThis(word, alphabet)).join(' ')
+    } else {
+      return words.map(word => decodeThis(word, alphabet)).join(' ')
+    }
+
+    function checkAlphabet(alphabet) {
+      let areUnique = true
+      const uniqueLetters = []
+      Array.from(alphabet).forEach(letter => {
+      if (uniqueLetters.includes(letter)) areUnique = false
+      uniqueLetters.push(letter)
+      })
+      return areUnique
+    }
+
+    function encodeThis(word, alphabet) {
+      const letters = Array.from(word)
+      return letters.reduce((encoded, letter) => {
+        if (firstAlphabet.includes(letter)) {
+          const index = firstAlphabet.indexOf(letter)
+          encoded += alphabet.charAt(index)
+        } else {
+          encoded += letter
+        }
+        return encoded
+      }, "")
+    }
+
+    function decodeThis(word, alphabet) {
+      const letters = Array.from(word)
+      return letters.reduce((decoded, letter) => {
+        if (alphabet.includes(letter)) {
+          const index = alphabet.indexOf(letter)
+          decoded += firstAlphabet.charAt(index)
+        } else {
+          decoded += letter
+        }
+        return decoded
+      }, "")
+    }
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,45 @@
 const expect = require("chai").expect
+const { substitution } = require("../src/substitution")
+
+describe("substitution", () => {
+    it("should encode a given word with the substituion method given an alphabet", () => {
+        const message = "thinkful"
+        const alphabet = "xoyqmcgrukswaflnthdjpzibev"
+        const actual = substitution(message, alphabet)
+        const expected = "jrufscpw"
+        expect(actual).to.equal(expected)
+    })
+    it("should encode a phrase with spaces using the substitution method with a given alphabet", () => {
+        const message = "You are an excellent spy"
+        const alphabet = "xoyqmcgrukswaflnthdjpzibev"
+        const actual = substitution(message, alphabet)
+        const expected = "elp xhm xf mbymwwmfj dne"
+        expect(actual).to.equal(expected)
+    })
+    it("should be able to handle special characters in the input and the alphabet", () => {
+        const message = "Hi!"
+        const alphabet = "xoyqmcgr&kswaflnthdjpzibev"
+        const actual = substitution(message, alphabet)
+        const expected = "r&!"
+        expect(actual).to.equal(expected)
+    })
+    it("should return false if the given alphabet isn't exactly 26 character long", () => {
+        const message = "message"
+        const shortAlphabet = "abc"
+        const actual = substitution(message, shortAlphabet)
+        expect(actual).to.false
+    })
+    it("should return false if not all of the characters in the given alphabet are unique", () => {
+        const message = "message"
+        const repeatAlphabet = "aacdefghijklmnopqrstuvwxyz"
+        const actual = substitution(message, repeatAlphabet)
+        expect(actual).to.false
+    })
+    it("correctly decodes the given phrase, based on the alphabet given to the function", () => {
+        const message = "y&ii$r&"
+        const alphabet = "$wae&zrdxtfcygvuhbijnokmpl"
+        const actual = substitution(message, alphabet, false)
+        const expected = "message"
+        expect(actual).to.equal(expected)
+    })
+})
